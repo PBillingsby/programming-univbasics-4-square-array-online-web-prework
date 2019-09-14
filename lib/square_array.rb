4e985358afecdeb88904d1abfd17c936078a9bc8
Paul Billingsby <paulbillingsby@gmail.com>
2019-09-14 01:02:57 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  total = 0
+  while array[total] < array[-1] do
+    new_array = []
+    for i in array
+      total += 1
+      new_array += i
+      puts new_array
+    end
+  end
 end
